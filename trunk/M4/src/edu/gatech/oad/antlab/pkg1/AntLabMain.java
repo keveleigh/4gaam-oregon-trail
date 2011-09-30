b0972f9ac376927730fb9ab8ea05913cf301a010
stekoshy@gmail.com <stekoshy@gmail.com@dd5506f7-c07e-9ee0-cb96-eeb355b08bd3>
2011-09-30 22:24:53 +0000

    Added my name and GTID (Stefan)

--- trunk/M4/src/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ trunk/M4/src/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -73,8 +73,8 @@ import edu.gatech.oad.antlab.pkg2.*;
           toPrint += p4.toString("902685950");
 		  //Person5 replace P5 with your name
           //and ggg000x with your gt num
-          Person5 p5 = new Person5("P5");
-          toPrint += p5.toString("ggg005x");
+          Person5 p5 = new Person5("Stefan Koshy");
+          toPrint += p5.toString("902747462");
         System.out.println(toPrint);
         
         
