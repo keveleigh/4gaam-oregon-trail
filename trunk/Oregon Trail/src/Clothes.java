b982d6028813d23179d62702d554378bec2b2bb8
stekoshy@gmail.com <stekoshy@gmail.com@dd5506f7-c07e-9ee0-cb96-eeb355b08bd3>
2011-10-11 20:20:12 +0000

    Clothes class

---
+++ trunk/Oregon Trail/src/Clothes.java
@@ -0,0 +1,17 @@
+
+public class Clothes extends Item {
+	
+	public Clothes() {
+		price = 10;
+		quantity = 0;
+		name = "Clothes";
+		weight = 20;
+	}
+
+	@Override
+	public void use() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
