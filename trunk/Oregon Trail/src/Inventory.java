b24b56ea601d939b03b6bf54469a7074f50bc56d
darylhalima1@gmail.com <darylhalima1@gmail.com@dd5506f7-c07e-9ee0-cb96-eeb355b08bd3>
2011-10-12 05:19:19 +0000

    Daryl

---
+++ trunk/Oregon Trail/src/Inventory.java
@@ -0,0 +1,56 @@
+import java.util.*;
+
+/**
+ * The Class Inventory.
+ */
+public class Inventory {
+
+	/** The items. */
+	ArrayList<Item> items;
+
+	/** The current weight. */
+	int currentWeight;
+
+	/**
+	 * Instantiates a new inventory.
+	 *
+	 * @param items the items list
+	 * @param currentWeight the current weight
+	 */
+	public Inventory(ArrayList<Item> items, int currentWeight){
+		this.items = items;
+		this.currentWeight = currentWeight;
+	}
+
+	/**
+	 * Adds an item
+	 *
+	 * @param item the item
+	 */
+	public void add(Item item){
+		for (int index = 0; index <= items.size(); index++){
+			if (items.get(index) == item){
+				item.quantity++;
+			}
+			else if (items.size()+1 == index){
+				items.add(item);
+			}
+		}
+	}
+
+	/**
+	 * Removes an item
+	 *
+	 * @param item the item
+	 */
+	public void remove(Item item){
+		for (int index = 0; index <= items.size(); index++){
+			if (items.get(index) == item){
+				item.quantity--;
+			}
+			else if (items.size()+1 == index){
+				items.remove(item);
+			}
+		}
+	}
+}
