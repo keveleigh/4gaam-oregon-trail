6bfb3c41d4b0675daaab2ab756d2d2723834067d
jestenmann@gmail.com <jestenmann@gmail.com@dd5506f7-c07e-9ee0-cb96-eeb355b08bd3>
2011-10-20 14:11:53 +0000

    Added Javadocs for presentation

--- trunk/Oregon Trail/src/GameInterface.java
+++ trunk/Oregon Trail/src/GameInterface.java
@@ -4,13 +4,30 @@ import java.util.ArrayList;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+/**
+ * The Game Interface, which displays the screen while
+ * the user is taking a turn and updates accordingly
+ * 
+ * @author Four Guys and a Mann
+ * @version v0.6 10/20/11
+ */
 public class GameInterface extends JFrame {
 
+    /** The display. */
     private JPanel display;
+    
+    /** The leader. */
     private Leader leader;
+    
+    /** The wagon. */
     private Wagon wagon;
+    
+    /** The members. */
     private ArrayList<Member> members;
 
+    /**
+     * Instantiates a new game interface.
+     */
     public GameInterface() {
         display = new configScreen(this);
         display.setSize(609, 311);
@@ -23,6 +40,11 @@ public class GameInterface extends JFrame {
         add(display);
     }
     
+    /**
+     * Change display.
+     *
+     * @param panel the panel
+     */
     public void changeDisplay(JPanel panel){
         remove(display);
         display = panel;
@@ -30,10 +52,21 @@ public class GameInterface extends JFrame {
         validate();
     }
     
+    /**
+     * Update bounds.
+     *
+     * @param width the width
+     * @param height the height
+     */
     public void updateBounds(int width, int height){
         setBounds(100, 100, width + 60, height + 20);
     }
 
+    /**
+     * The main method.
+     *
+     * @param args the arguments
+     */
     public static void main(String[] args) {
         EventQueue.invokeLater(new Runnable() {
 
@@ -50,6 +83,8 @@ public class GameInterface extends JFrame {
     }
 
     /**
+     * Gets the leader.
+     *
      * @return the leader
      */
     public Leader getLeader() {
@@ -57,6 +92,8 @@ public class GameInterface extends JFrame {
     }
 
     /**
+     * Sets the leader.
+     *
      * @param leader the leader to set
      */
     public void setLeader(Leader leader) {
@@ -64,6 +101,8 @@ public class GameInterface extends JFrame {
     }
 
     /**
+     * Gets the wagon.
+     *
      * @return the wagon
      */
     public Wagon getWagon() {
@@ -71,6 +110,8 @@ public class GameInterface extends JFrame {
     }
 
     /**
+     * Sets the wagon.
+     *
      * @param wagon the wagon to set
      */
     public void setWagon(Wagon wagon) {
@@ -78,6 +119,8 @@ public class GameInterface extends JFrame {
     }
 
     /**
+     * Gets the members.
+     *
      * @return the members
      */
     public ArrayList<Member> getMembers() {
@@ -85,6 +128,8 @@ public class GameInterface extends JFrame {
     }
 
     /**
+     * Sets the members.
+     *
      * @param members the members to set
      */
     public void setMembers(ArrayList<Member> members) {
