3b688d329604517ad69512384ee5cbf5b3ec7f3b
kurtis.eveleigh@gmail.com <kurtis.eveleigh@gmail.com@dd5506f7-c07e-9ee0-cb96-eeb355b08bd3>
2011-10-20 08:00:41 +0000

    Edited the Shop and Inventory classes to work better with the store GUI and each other. Also, removed the item cap on store inventories (sorry for the extra work Jesten).

--- trunk/Oregon Trail/src/Inventory.java
+++ trunk/Oregon Trail/src/Inventory.java
@@ -2,6 +2,9 @@ import java.util.*;
 
 /**
  * The Class Inventory.
+ * 
+ * @author Four Guys and a Mann
+ * @version v1.1 10/20/11
  */
 public class Inventory {
 
@@ -13,7 +16,23 @@ public class Inventory {
 	
 	/** The max weight. */
 	//the max weight may be changed, still need to decide on this
-	static int MAX_WEIGHT = 1000;
+	int MAX_WEIGHT = 1000;
+	
+	/**
+	 * Instantiates a new inventory.
+	 */
+	public Inventory()
+	{
+		this.items = new ArrayList<Item>();
+		items.add(new Axle());
+		items.add(new Bullets());
+		items.add(new Clothes());
+		items.add(new Food());
+		items.add(new Oxen());
+		items.add(new Tongue());
+		items.add(new Wheels());
+		currentWeight = 0;
+	}
 	
 	/**
 	 * Instantiates a new inventory.
@@ -32,16 +51,22 @@ public class Inventory {
 	 * @param item the item
 	 */
 	public void add(Item item){
-		for (int index = 0; index <= items.size(); index++){
-			//checks to see if item is already in inventory
-			if (items.get(index).name.equals(item.name)){
-				items.get(index).quantity++;
-				currentWeight = currentWeight + item.weight;
-			}
-			//if item isn't in inventory yet
-			else if (items.size()+1 == index){
-				items.add(item);
-				currentWeight = currentWeight + item.weight;
+		if (currentWeight + item.getTotalWeight() > MAX_WEIGHT)
+		{
+			System.out.println("Your inventory is full.");
+		}
+		else
+		{
+			for (int index = 0; index < items.size(); index++)
+			{
+				//checks to see if item is already in inventory
+				Item current = items.get(index);
+				if ((current.name).equals(item.name))
+				{
+					current.quantity += item.getQuantity();
+					currentWeight = currentWeight + item.getTotalWeight();
+					System.out.println("Items successfully added.");
+				}
 			}
 		}
 	}
@@ -51,17 +76,49 @@ public class Inventory {
 	 *
 	 * @param item the item
 	 */
-	public void remove(Item item){
-		for (int index = 0; index <= items.size(); index++){
-			if (items.get(index).name.equals(item.name)){
-				items.get(index).quantity--;
-				currentWeight = currentWeight - item.weight;
-			}
-			else if (items.size()+1 == index){
-				items.remove(item);
-				currentWeight = currentWeight - item.weight;
+	public void remove(Item item)
+	{
+		if (currentWeight == 0)
+		{
+			System.out.println("Your inventory is empty.");
+		}
+		else
+		{
+			for (int index = 0; index < items.size(); index++)
+			{
+				Item current = items.get(index);
+				if ((current.name).equals(item.name))
+				{
+					if (current.getQuantity() < item.getQuantity())
+					{
+						System.out.println("You don't have that many items.");
+					}
+					else
+					{	
+						current.quantity -= item.getQuantity();
+						currentWeight = currentWeight - item.getTotalWeight();
+						System.out.println("Items successfully removed.");
+					}
+				}
 			}
 		}
 	}
 
+	/**
+	 * Checks how many of an item are stored.
+	 * 0 = axle
+	 * 1 = bullets
+	 * 2 = clothes
+	 * 3 = food
+	 * 4 = oxen
+	 * 5 = tongue
+	 * 6 = wheels
+	 *
+	 * @param int The amount of the item stored.
+	 */
+	public int getQuantity(int i)
+	{
+		Item currentItem = items.get(i);
+		return currentItem.quantity;
+	}
 }
