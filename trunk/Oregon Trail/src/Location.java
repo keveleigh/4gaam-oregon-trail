d2cfac3f13fb9afa19454d2c4e00477d86d46203
darylhalima1@gmail.com <darylhalima1@gmail.com@dd5506f7-c07e-9ee0-cb96-eeb355b08bd3>
2011-11-01 18:47:25 +0000

    Stefan - Edited Location class to have a "lastLocation" parameter

--- trunk/Oregon Trail/src/Location.java
+++ trunk/Oregon Trail/src/Location.java
@@ -2,10 +2,16 @@
 public class Location {
 	String name;
 	int distance; //distance from beginning
+	boolean lastOne;
 	
 	public Location(String name, int distance){
 		this.name = name;
 		this.distance = distance;
+	}
+	
+	public Location(String name, int distance, boolean lastOne) {
+		this(name, distance);
+		this.lastOne = lastOne;
 	}
 
 	public String getName() {
@@ -14,5 +20,20 @@ public class Location {
 	
 	public int getDistance() {
 		return distance;
+	}
+	
+	public boolean isLast() {
+		if (lastOne == true) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public boolean equals(Location loc2) {
+		if (name == loc2.getName() && distance == loc2.getDistance())
+			return true;
+		else
+			return false;
 	}
 }
