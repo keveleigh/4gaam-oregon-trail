b3977ae53f7bf7a7662d5ac8877d7ea2d36c42ff
darylhalima1@gmail.com <darylhalima1@gmail.com@dd5506f7-c07e-9ee0-cb96-eeb355b08bd3>
2011-11-01 18:51:16 +0000

    Stefan - Added JavaDoc

--- trunk/Oregon Trail/src/Location.java
+++ trunk/Oregon Trail/src/Location.java
@@ -1,27 +1,65 @@
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Location.
+ */
 public class Location {
+	
+	/** The name. */
 	String name;
+	
+	/** The distance. */
 	int distance; //distance from beginning
+	
+	/** The last one. */
 	boolean lastOne;
 	
+	/**
+	 * Instantiates a new location.
+	 *
+	 * @param name the name
+	 * @param distance the distance
+	 */
 	public Location(String name, int distance){
 		this.name = name;
 		this.distance = distance;
 	}
 	
+	/**
+	 * Instantiates a new location.
+	 *
+	 * @param name the name
+	 * @param distance the distance
+	 * @param lastOne the last one
+	 */
 	public Location(String name, int distance, boolean lastOne) {
 		this(name, distance);
 		this.lastOne = lastOne;
 	}
 
+	/**
+	 * Gets the name.
+	 *
+	 * @return the name
+	 */
 	public String getName() {
 		return name;
 	}
 	
+	/**
+	 * Gets the distance.
+	 *
+	 * @return the distance
+	 */
 	public int getDistance() {
 		return distance;
 	}
 	
+	/**
+	 * Checks if is last.
+	 *
+	 * @return true, if is last
+	 */
 	public boolean isLast() {
 		if (lastOne == true) {
 			return true;
@@ -30,6 +68,12 @@ public class Location {
 		}
 	}
 	
+	/**
+	 * Equals.
+	 *
+	 * @param loc2 the loc2
+	 * @return true, if successful
+	 */
 	public boolean equals(Location loc2) {
 		if (name == loc2.getName() && distance == loc2.getDistance())
 			return true;
--- trunk/Oregon Trail/src/Turn.java
+++ trunk/Oregon Trail/src/Turn.java
@@ -1,21 +1,34 @@
 import javax.swing.*;
 
 
+// TODO: Auto-generated Javadoc
 /**
  * The Class Turn, which allows the user to take a turn
- * and updates the rations, pace, and distance accordingly
- * 
+ * and updates the rations, pace, and distance accordingly.
+ *
  * @author Four Guys and a Mann
  * @version v0.6 10/20/11
  */
 
 public class Turn {
 
+    /** The wagon. */
     private Wagon wagon;
+    
+    /** The wagon load. */
     private Inventory wagonLoad;
+    
+    /** The distance moved. */
     private int distanceMoved;
+    
+    /** The current food. */
     private int currentFood;
 
+    /**
+     * Instantiates a new turn.
+     *
+     * @param wagon the wagon
+     */
     public Turn(Wagon wagon) {
         this.wagon = wagon;
         wagonLoad = wagon.getWagonLoad();
@@ -23,6 +36,11 @@ public class Turn {
         currentFood = wagonLoad.getQuantity(3);
     }
 
+    /**
+     * Take turn.
+     *
+     * @param mapClass the map class
+     */
     public void takeTurn(Map mapClass) {
         Location lastTown = mapClass.getLastLoc();
         distanceMoved += wagon.getCurrPace().getPace();
@@ -38,17 +56,26 @@ public class Turn {
     }
 
     /**
+     * Gets the distance moved.
+     *
      * @return the distanceMoved
      */
     public int getDistanceMoved() {
         return distanceMoved;
     }
     
+    /**
+     * Sets the distance moved.
+     *
+     * @param dist the new distance moved
+     */
     public void setDistanceMoved(int dist) {
     	distanceMoved = dist;
     }
 
     /**
+     * Gets the current food.
+     *
      * @return the currentFood
      */
     public int getCurrentFood() {
