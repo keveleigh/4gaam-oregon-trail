c515d20129ba5e8b38933f566c17f965a429aa98
darylhalima1@gmail.com <darylhalima1@gmail.com@dd5506f7-c07e-9ee0-cb96-eeb355b08bd3>
2011-11-08 05:24:30 +0000

    Daryl

---
+++ trunk/Oregon Trail/src/River.java
@@ -0,0 +1,67 @@
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class River.
+ */
+public class River extends Location{
+	
+	/** The name. */
+	private static String name;
+	
+	/** The distance. */
+	private static int depth, width, distance;
+	
+	/**
+	 * Instantiates a new river.
+	 *
+	 * @param name the name
+	 * @param depth the depth
+	 * @param width the width
+	 * @param distance the distance
+	 */
+	public River(String name, int depth, int width, int distance){
+		super(name, distance);
+		this.depth = depth;
+		this.width = width;
+	}
+
+	/**
+	 * Gets the depth.
+	 *
+	 * @return the depth
+	 */
+	public static int getDepth() {
+		return depth;
+	}
+
+	/**
+	 * Sets the depth.
+	 *
+	 * @param depth the new depth
+	 */
+	public static void setDepth(int depth) {
+		River.depth = depth;
+	}
+
+	/**
+	 * Gets the width.
+	 *
+	 * @return the width
+	 */
+	public static int getWidth() {
+		return width;
+	}
+
+	/**
+	 * Sets the width.
+	 *
+	 * @param width the new width
+	 */
+	public static void setWidth(int width) {
+		River.width = width;
+	}
+	
+	public boolean disasterCheck(){
+		return false;
+	}
+}
