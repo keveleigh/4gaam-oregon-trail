1d231daa1360c5f5f5b27c093c78b7436b906cf8
darylhalima1@gmail.com <darylhalima1@gmail.com@dd5506f7-c07e-9ee0-cb96-eeb355b08bd3>
2011-12-08 20:38:59 +0000

    Added a total of 7 more random events

--- trunk/Oregon Trail/src/randomEvent.java
+++ trunk/Oregon Trail/src/randomEvent.java
@@ -17,15 +17,17 @@ public class randomEvent {
 
 	public int generateEvent(){
 		/*
-		 * 0 is getSick, 1 is weather, 2 is oxDead, 3 is foundItem, 4 nothing happens
+		 * 0 is getSick, 1 is getWell, 2 is weather, 3 is oxDead, 4 is foundItem, 
+		 * 5 loseItem, 6 is wagonDamage, 7 is oxStolen, 8 is getEmotional, 
+		 * 9 is newMember, 10 is tribeEncounter, 11 nothing happens
 		 */
 		if (Event()){
-			return random.nextInt(10);
+			return random.nextInt(12);
 		} else {
 			return 4;
 		}
 	}
-	
+
 	public String getSick() {
 		/*
 		 * 0 is a broken arm, 1 is a case of the flu, 2 is severe diarrhea
@@ -47,11 +49,11 @@ public class randomEvent {
 			return "Your members have just survived a terrible storm.  Nobody got sick.";
 		}
 	}
-	
+
 	public String getWell(){
 		return getWell(random.nextInt(3), random.nextInt(4));
 	}
-	
+
 	public String getWell(int wellType, int person){
 		if (wellType == 0){
 			Wagon.getMembers().get(person).getMyHealth().increase(100);
@@ -61,14 +63,14 @@ public class randomEvent {
 			return "You have just found a healing herb and give it to one of your group members.";
 		}
 	}
-	
+
 	public String weather() {
 		/*
 		 * 0 is thunderstorm, 1 is a sandstorm, 2 is hail, 3 is safe
 		 */
 		return weather(random.nextInt(4));
 	}
-	
+
 	public String weather(int weather){
 		if (weather ==0){
 			getSick();
@@ -91,7 +93,7 @@ public class randomEvent {
 		Wagon.getWagonLoad().items.get(4).decrement();
 		return "A snake has bitten one of your oxen and killed it.";
 	}
-	
+
 	public String foundItem(){
 		int itemType = random.nextInt(5);
 		if (itemType == 0){
@@ -117,11 +119,11 @@ public class randomEvent {
 			return "You have found an ox, but it ran away before you could control it.";
 		}
 	}
-	
+
 	public String loseItem(){
 		return loseItem(random.nextInt(5), random.nextInt(2));
 	}
-	
+
 	public String loseItem(int itemType, int loseType){ //needs work
 		Inventory inventory = Wagon.getWagonLoad();
 		if (loseType ==0){
@@ -132,7 +134,7 @@ public class randomEvent {
 			return "A thief has robbed " + inventory.getItems().get(itemType).name + " during your sleep.";
 		}
 	}
-	
+
 	public String wagonDamage(){
 		int part = random.nextInt(4);
 		if (part == 0){
@@ -148,7 +150,7 @@ public class randomEvent {
 			return "You have just went through a massive pothole.  You are lucky that your wagon's still fine.";
 		}
 	}
-	
+
 	public String oxStolen(){
 		int chance = random.nextInt(8);
 		if (chance == 0){			
@@ -162,7 +164,7 @@ public class randomEvent {
 			return "You wake up in the middle of the night to a bandit trying to steal your oxen and stop him.";
 		}
 	}
-	
+
 	public String getEmotional(){
 		int chance = random.nextInt(5);
 		if (chance ==0){
@@ -175,6 +177,32 @@ public class randomEvent {
 			return "You start to cry over the fact that you're still not at Oregon.";
 		} else {
 			return "You are about to cry but tell yourself that you're not going to cry no matter what.";
+		}
+	}
+
+	public String newMember(){
+		if (Wagon.getMembers().size() < 4){
+			Member newMember = new Member("Jorge");
+			Wagon.getMembers().add(newMember);
+			return "You have just met Jorge, a wandering soul in search of adventure.  He joins your group!  Unfortunately he doesn't have any special skills.";
+		} else {
+			return "You have just met Jorge, a wandering soul in search of adventure.  He wants to join your group but your party is full at the moment.";
+		}
+	}
+	
+	public String tribeEncounter(){
+		return tribeEncounter(random.nextInt(3), random.nextInt(4));
+	}
+	public String tribeEncounter(int type, int person){
+		if (type ==0){
+			Wagon.getMembers().remove(person);
+			return "You have just met a tribe.  They turn out to be cannibals, and you lose " + Wagon.getMembers().get(person) + " to them."; 
+		} else if (type ==1){
+			Food food = new Food(100);
+			Wagon.getWagonLoad().add(food);
+			return "You have just met a tribe.  They turn out to be very welcoming and give you 100 pounds of food.";
+		} else {
+			return "You have just met a tribe.  They turn out to be very friendly but you said an inappropriate joke, so they shun you away.";
 		}
 	}
 }
